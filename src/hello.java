b6e7637259d88cf72b58f06e97cba1f6a667d4bc
Tannnnnn <tan_sweetmulet@hotmail.com>
2017-09-18 13:52:17 +0700

    :love: :poop:

--- src/hello.java
+++ src/hello.java
@@ -5,7 +5,7 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
-		System.out.print("Enter Number to check Odd or Even Number : ");
+		System.out.print("Enter Number to find Circle Area : ");
 		int num = in.nextInt();
 		
 		Circle circle = new Circle() ;
