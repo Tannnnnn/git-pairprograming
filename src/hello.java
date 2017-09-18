c32eae9208ee0aec6409d3e8380fc89943752983
Harit <slipknot.opal@gmail.com>
2017-09-18 14:15:03 +0700

    master

--- src/hello.java
+++ src/hello.java
@@ -7,7 +7,7 @@ public class hello {
 		Scanner in = new Scanner(System.in);
 		System.out.print("Enter Number to check Odd or Even Number : ");
 		int num = in.nextInt();
-		
+		 
 		isEven isEven = new isEven() ;
 		if(isEven.isEven(num))
 		{
