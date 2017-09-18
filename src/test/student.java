15d46bdf5fecfeb7b49e1467e5b46751c771f44c
Tannnnnn <tan_sweetmulet@hotmail.com>
2017-09-18 14:37:03 +0700

    score

---
+++ src/test/student.java
@@ -0,0 +1,27 @@
+package test;
+
+import java.util.Scanner;
+
+public class student {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter Number to check Odd or Even Number : ");
+		int score = in.nextInt();
+		 
+		if(score<0)
+		{
+			System.out.print("less than");
+		}
+		else if(score>100)
+		{
+			System.out.print("more than");
+		}
+		else
+		{
+			System.out.print(score);
+		}
+		
+	}
+
+}
