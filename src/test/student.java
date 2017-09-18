a677907715349f08822e17bbda1ed6eccc3683b7
Harit <slipknot.opal@gmail.com>
2017-09-18 14:41:06 +0700

    success

--- src/test/student.java
+++ src/test/student.java
@@ -19,7 +19,22 @@ public class student {
 		}
 		else
 		{
-			System.out.print("ok");
+			if(score>=80)
+				System.out.print("Grade:A");
+			else if(score>=75)
+				System.out.print("Grade:B+");
+			else if(score>=70)
+				System.out.print("Grade:B");
+			else if(score>=65)
+				System.out.print("Grade:C+");
+			else if(score>=60)
+				System.out.print("Grade:C");
+			else if(score>=55)
+				System.out.print("Grade:D+");
+			else if(score>=50)
+				System.out.print("Grade:D");
+			else
+				System.out.print("Grade:F");
 		}
 		
 	}
